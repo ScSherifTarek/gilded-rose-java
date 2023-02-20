a65b21db15bd973f184cdb057710cdbae0bee51f
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2023-02-20 22:16:54 +0200

    Extract Backstage Passes Logic to a method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,31 +16,14 @@ class GildedRose {
             if(item.name.equals(ITEM_NAME_AGED_BRIE)) {
                 this.handleAgedBrie(item);
                 continue;
+            } else if (item.name.equals(ITEM_NAME_BACKSTAGE_PASSES)) {
+                this.handleBackstagePasses(item);
+                continue;
             }
 
-            if (!item.name.equals(ITEM_NAME_BACKSTAGE_PASSES)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(ITEM_NAME_SULFURAS)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(ITEM_NAME_BACKSTAGE_PASSES)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
+            if (item.quality > 0) {
+                if (!item.name.equals(ITEM_NAME_SULFURAS)) {
+                    item.quality = item.quality - 1;
                 }
             }
 
@@ -49,14 +32,10 @@ class GildedRose {
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(ITEM_NAME_BACKSTAGE_PASSES)) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals(ITEM_NAME_SULFURAS)) {
-                            item.quality = item.quality - 1;
-                        }
+                if (item.quality > 0) {
+                    if (!item.name.equals(ITEM_NAME_SULFURAS)) {
+                        item.quality = item.quality - 1;
                     }
-                } else {
-                    item.quality = item.quality - item.quality;
                 }
             }
         }
@@ -71,6 +50,21 @@ class GildedRose {
         item.sellIn -= 1;
         if (item.sellIn < 0 && item.quality < 50) {
             item.quality = item.quality + 1;
+        }
+    }
+
+    private void handleBackstagePasses(Item item) {
+        item.sellIn -= 1;
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        } else if (item.sellIn < 5) {
+            item.quality = item.quality + 3;
+        } else if (item.sellIn < 10) {
+            item.quality = item.quality + 2;
+        }
+
+        if (item.quality > 50) {
+            item.quality = 50;
         }
     }
 }
