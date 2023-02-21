af8ff30392b4d15f6e8a9a329ff84fe3f91741ba
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2023-02-21 20:28:40 +0200

    Use += instead of var = var + value

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -30,12 +30,12 @@ class GildedRose {
     private void handleAgedBrie(Item item) {
         
         if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            item.quality += 1;
         }
 
         item.sellIn -= 1;
         if (item.sellIn < 0 && item.quality < 50) {
-            item.quality = item.quality + 1;
+            item.quality += 1;
         }
     }
 
@@ -43,9 +43,9 @@ class GildedRose {
         if (item.sellIn <= 0) {
             item.quality = 0;
         } else if (item.sellIn <= 5) {
-            item.quality = item.quality + 3;
+            item.quality += 3;
         } else if (item.sellIn <= 10) {
-            item.quality = item.quality + 2;
+            item.quality += 2;
         }
 
         item.sellIn -= 1;
